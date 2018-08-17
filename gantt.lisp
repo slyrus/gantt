bc46b5c5239149d4972a03eba3b8486daa238018
Cyrus Harmon <cyrus@bobobeach.com>
2018-08-17 16:49:23 -0700

    WIP: add mermaid output

--- gantt.lisp
+++ gantt.lisp
@@ -4,7 +4,9 @@
   (:import-from :local-time
                 #:encode-timestamp
                 #:timestamp+
-                #:timestamp+)
+                #:timestamp+
+                #:timestamp-minimum
+                #:timestamp-maximum)
   (:import-from :time-interval
                 #:time-interval)
   (:import-from :cl-who
@@ -248,7 +250,7 @@
       (reduce (lambda (&optional a b)
                 (cond ((null a) b)
                       ((null b) a)
-                      (t (min a b))))
+                      (t (timestamp-minimum a b))))
               (let ((children (children task)))
                 (map (type-of children) #'start children)))))
 
@@ -257,7 +259,7 @@
       (reduce (lambda (&optional a b)
                 (cond ((null a) b)
                       ((null b) a)
-                      (t (max a b))))
+                      (t (timestamp-maximum a b))))
               (let ((children (children task)))
                 (map (type-of children) #'end children)))))
 
@@ -311,3 +313,29 @@
            (cdr task-spec))
       task)))
 
+(defun write-mermaid-file (file task &key (show-finished-tasks t)
+                                          (show-start t)
+                                          (show-end t)
+                                          (show-resources t)
+                                          (show-dependencies nil)
+                                          (row-colors *row-colors*))
+  (declare (ignore show-finished-tasks show-start show-end show-resources show-dependencies row-colors))
+  (with-open-file (s file :direction :output :if-does-not-exist :create :if-exists :supersede)
+    (format s "gantt")
+    (format s "~&dateFormat  YYYY-MM-DD")
+    (format s "~&title ~A~%~%" (name task))
+
+    (labels ((%print-task-tree-mermaid (task &key leaf start)
+               (print (list (name task) (length (children task))))
+               (if (plusp (length (children task)))
+                   (loop :for child :across (children task)
+                      :do (%print-task-tree-mermaid child :leaf t :start (or (start task) start)))
+                   (progn
+                     (if leaf
+                         (let ((start (or (start task) start)))
+                           (when start
+                             (format s "~A : ~A,~A~&" (name task)
+                                     (local-time:format-timestring nil start :format local-time:+iso-8601-date-format+)
+                                     (local-time:format-timestring nil start :format local-time:+iso-8601-date-format+))))
+                         (format s "~&section ~A~&" (name task)))))))
+      (%print-task-tree-mermaid task :start (start task)))))
