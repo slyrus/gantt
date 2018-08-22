9ca56612bffd03412cc214beb2da85366767b2ea
Cyrus Harmon <cyrus@bobobeach.com>
2018-08-21 18:34:32 -0700

    improve mermaid output

--- mermaid.lisp
+++ mermaid.lisp
@@ -37,26 +37,38 @@
     (format s "~&dateFormat  YYYY-MM-DD")
     (format s "~&title ~A~%~%" (name task))
 
-    (labels ((%print-task-tree-mermaid (task &key leaf start (level 0))
+    (labels ((%print-task-tree-mermaid (task &key start (level 0))
                (print (list (name task) (length (children task))))
                (if (plusp (length (children task)))
-                   (loop :for child :across (children task)
-                      :do (%print-task-tree-mermaid child
-                                                    :leaf t
-                                                    :start (or (start task) start)
-                                                    :level (1+ level)))
                    (progn
-                     (if leaf
-                         (let ((start (or (start task) start))
-                               (duration (duration task)))
-                           (when (and start
-                                      duration
-                                      (local-time:timestamp> start start-cutoff-date))
-                             (format s "~A :~A,~A,~A,~A~&"
-                                     (name task)
-                                     "active"
-                                     (id task)
-                                     (local-time:format-timestring nil start :format local-time:+iso-8601-date-format+)
-                                     (mermaid-print-time-interval duration))))
-                         (format s "~&section ~A~&" (name task)))))))
-      (%print-task-tree-mermaid task :start (start task)))))
+                     (format s "~&section ~A~%" (name task))
+                     (loop :for child :across (children task)
+                        :do (%print-task-tree-mermaid child
+                                                      :start (or (task-start task) start)
+                                                      :level (1+ level))))
+                   (let ((start (or (task-start task) start))
+                         (duration (duration task))
+                         (prereqs (gantt::prerequisite-tasks task)))
+                     (when (and (or start prereqs)
+                                duration
+                                (and start (local-time:timestamp> start start-cutoff-date)))
+                       (format s "~&~A :"
+                               (name task))
+                       (format s "~@[~A,~]"
+                               (cond ((and (task-progress task)
+                                           (>= (task-progress task) 1.0))
+                                      "done")
+                                     ((and (task-progress task)
+                                           (< (task-progress task) 1.0)
+                                           (> (task-progress task) 0))
+                                      "active")
+                                     (t  nil)))
+                       (format s "~A,"
+                               (id task))
+                       (if prereqs
+                           (format s "after ~A," (id (car (last-ending-task prereqs))))
+                           (format s "~A,"
+                                   (local-time:format-timestring nil start :format local-time:+iso-8601-date-format+)))
+                       (format s "~A~&"
+                               (mermaid-print-time-interval duration)))))))
+      (%print-task-tree-mermaid task :start (task-start task)))))
